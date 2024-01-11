be4e4bfdb5ce65df6105e2dec8bb43d008f698c3
shadinab <shadinabwani2000@gmail.com>
2024-01-11 11:22:44 +0200

    11.01.2024-v1

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -2,10 +2,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
 const apiService = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
