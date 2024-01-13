5f7fd0dfb1a17d0f2e0fef4f0195ea59a0e68574
shadinab <shadinabwani2000@gmail.com>
2024-01-13 23:41:25 +0200

    chat3

--- server.js
+++ server.js
@@ -57,10 +57,18 @@ const { Server } = require('socket.io');
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://steady-dasik-0636f2.netlify.app/',
     methods: ['GET', 'POST'],
   },
 });
+
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST'],
+//   },
+// });
 
 io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`);
