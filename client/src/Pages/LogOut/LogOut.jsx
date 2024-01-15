4674b0ec429885efb76278c46796ebaaaee32cb4
shadinab <shadinabwani2000@gmail.com>
2024-01-15 20:32:36 +0200

    15.01.2024-v1

--- client/src/Pages/LogOut/LogOut.jsx
+++ client/src/Pages/LogOut/LogOut.jsx
@@ -1,48 +1,85 @@
+
+
 // Logout.jsx
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Spinner from '../../utils/Spinner';
 
 const Logout = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const handleLogout = () => {
-      localStorage.removeItem('tokenconnectionId');
-      localStorage.removeItem('selectedUserId');
-      navigate('/');
-      window.location.reload(); // Reload the page
+      try {
+        localStorage.removeItem('tokenconnectionId');
+        localStorage.removeItem('selectedUserId');
+
+            // Navigate and reload the page after the toast
+            navigate('/');
+            window.location.reload(); // Reload the page
+            setLoading(false);
+
+      } catch (error) {
+        console.error('Logout failed', error);
+        setLoading(false);
+      }
     };
 
     // Execute the logout action immediately when the component mounts
-    handleLogout();
+    handleLogout(          
+);
   }, [navigate]); // Add navigate as a dependency to useEffect
 
   // Return null (no visible content)
-  return null;
+  return (
+    <>
+      <Spinner loading={loading} />
+    </>
+  );
 };
 
 export default Logout;
 
-// Logout.jsx
+
+
+// // Logout.jsx
+// import { useEffect, useState } from 'react';
 // import { useNavigate } from 'react-router-dom';
-// import './LogOut.css'
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import Spinner from '../../utils/Spinner';
 
 // const Logout = () => {
 //   const navigate = useNavigate();
+//   const [loading, setLoading] = useState(true);
 
-//   const handleLogout = () => {
-//     localStorage.removeItem('tokenconnectionId');
-//     localStorage.removeItem('selectedUserId');
+//   useEffect(() => {
+//     const handleLogout = () => {
+//       try {
+//         localStorage.removeItem('tokenconnectionId');
+//         localStorage.removeItem('selectedUserId');
+//         toast.success('Logout successful!'); // Display success toast
 //         navigate('/');
-//     window.location.reload(); // Reload the page
-//   };
+//         window.location.reload(); // Reload the page
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Logout failed', error);
+//         toast.error('Logout failed. Please try again.'); // Display error toast
+//         setLoading(false);
+//       }
+//     };
 
+//     // Execute the logout action immediately when the component mounts
+//     handleLogout();
+//   }, [navigate]); // Add navigate as a dependency to useEffect
+
+//   // Return null (no visible content)
 //   return (
-//     <div className="logout-container">
-//       <button className="navLink center" onClick={handleLogout}>
-//         You have successfully logged out <br/><br/> Please Click Me!!!
-//       </button>
-//     </div>
+//     <>
+//       <ToastContainer />
+//       <Spinner loading={loading} />
+//     </>
 //   );
 // };
 
