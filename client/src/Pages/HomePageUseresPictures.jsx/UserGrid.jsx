162157dcd495de48110d8bc9feedaf4a454f84f9
shadinab <shadinabwani2000@gmail.com>
2024-01-16 23:52:36 +0200

    16.01.2024-vv10

--- client/src/Pages/HomePageUseresPictures.jsx/UserGrid.jsx
+++ client/src/Pages/HomePageUseresPictures.jsx/UserGrid.jsx
@@ -26,16 +26,7 @@ const UserGrid = () => {
     };
 
 
-    //  useEffect(() => {
-    // const fetchData = async () => {
-    //   try {
-    //     const response = await axios.get('http://localhost:5000/api/users');
-    //     setUsers(response.data.data);
-    //   } catch (error) {
-    //     console.error('Error fetching user data:', error.message);
-    //   }
-    // };
-
+ 
     fetchData();
 
     const handleResize = () => {
@@ -61,11 +52,8 @@ const UserGrid = () => {
     localStorage.setItem('selectedUserId', JSON.stringify(user.connectionId));
     setSelectedUser(user);
   };
-// const formatBirthday = (user) => {
-//   const originalDateString = user.birthday;
-//   const dateObject = new Date(originalDateString);
-//   return dateObject.toISOString().split('T')[0];
-// };
+
+
 
   return (
     <div className="user-grid">
@@ -81,7 +69,6 @@ const UserGrid = () => {
               <img src={user.avatar} alt={user.name} className="user-picture" />
               <div className="font">{user.name}</div>
               <div className="font">{user.preferences.ageRange}</div>
-              {/* <div className="font">{formatBirthday(user)}</div>{' '} */}
             </Link>
           </button>
         </div>
@@ -91,6 +78,28 @@ const UserGrid = () => {
 };
 
 export default UserGrid;
+              {
+                /* <div className="font">{formatBirthday(user)}</div>{' '} */
+              }
+
+   //  useEffect(() => {
+    // const fetchData = async () => {
+    //   try {
+    //     const response = await axios.get('http://localhost:5000/api/users');
+    //     setUsers(response.data.data);
+    //   } catch (error) {
+    //     console.error('Error fetching user data:', error.message);
+    //   }
+    // };
+
+
+// const formatBirthday = (user) => {
+//   const originalDateString = user.birthday;
+//   const dateObject = new Date(originalDateString);
+//   return dateObject.toISOString().split('T')[0];
+// };
+
+
 // import { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 // import axios from 'axios';
