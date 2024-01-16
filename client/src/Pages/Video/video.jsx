ccf2f7971b4b739f8148e57b55df9f8f7206fd33
shadinab <shadinabwani2000@gmail.com>
2024-01-16 20:25:54 +0200

    v2

--- client/src/Pages/Video/video.jsx
+++ client/src/Pages/Video/video.jsx
@@ -1,71 +1,71 @@
-import * as React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+// import * as React from 'react';
+// import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 
-const appID1 = JSON.parse(import.meta.env.VITE_APP_APP_ID);
-const serverSecret1 = import.meta.env.VITE_APP_SERVER_SECRET;
+// const appID1 = JSON.parse(import.meta.env.VITE_APP_APP_ID);
+// const serverSecret1 = import.meta.env.VITE_APP_SERVER_SECRET;
 
 
-function randomID(len) {
-  let result = '';
-  if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-    maxPos = chars.length,
-    i;
-  len = len || 5;
-  for (i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return result;
-}
+// function randomID(len) {
+//   let result = '';
+//   if (result) return result;
+//   var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+//     maxPos = chars.length,
+//     i;
+//   len = len || 5;
+//   for (i = 0; i < len; i++) {
+//     result += chars.charAt(Math.floor(Math.random() * maxPos));
+//   }
+//   return result;
+// }
 
-export function getUrlParams(
-  url = window.location.href
-) {
-  let urlStr = url.split('?')[1];
-  return new URLSearchParams(urlStr);
-}
+// export function getUrlParams(
+//   url = window.location.href
+// ) {
+//   let urlStr = url.split('?')[1];
+//   return new URLSearchParams(urlStr);
+// }
 
-export default function App() {
-      const roomID = getUrlParams().get('roomID') || randomID(5);
-      let myMeeting = async (element) => {
-     // generate Kit Token
-  const appID = appID1;
-  const serverSecret = serverSecret1;
+// export default function App() {
+//       const roomID = getUrlParams().get('roomID') || randomID(5);
+//       let myMeeting = async (element) => {
+//      // generate Kit Token
+//   const appID = appID1;
+//   const serverSecret = serverSecret1;
 
-    const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
-
-
-     // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      // start the call
-      zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url:
-              window.location.protocol +
-              '//' +
-              window.location.host +
-              window.location.pathname +
-              '?roomID=' +
-              roomID,
-          },
-        ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-        },
-      });
+//     const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
 
 
-  };
+//      // Create instance object from Kit Token.
+//       const zp = ZegoUIKitPrebuilt.create(kitToken);
+//       // start the call
+//       zp.joinRoom({
+//         container: element,
+//         sharedLinks: [
+//           {
+//             name: 'Personal link',
+//             url:
+//               window.location.protocol +
+//               '//' +
+//               window.location.host +
+//               window.location.pathname +
+//               '?roomID=' +
+//               roomID,
+//           },
+//         ],
+//         scenario: {
+//           mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+//         },
+//       });
 
-  return (
-    <div
-      className="myCallContainer"
-      ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
-    ></div>
-  );
-}
+
+//   };
+
+//   return (
+//     <div
+//       className="myCallContainer"
+//       ref={myMeeting}
+//       style={{ width: '100vw', height: '100vh' }}
+//     ></div>
+//   );
+// }
